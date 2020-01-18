55c9112e6382bde72a24361e129d93662fe02ca7
Shubam Virdi <Shubhamvirdic3@gmail.com>
2020-01-18 10:52:36 +0530

    commiting java class

--- app/src/main/java/com/sihlpu/smartpolice/ModelClasses/User.java
+++ app/src/main/java/com/sihlpu/smartpolice/ModelClasses/User.java
@@ -1,4 +1,57 @@
 package com.sihlpu.smartpolice.ModelClasses;
 
 public class User {
+
+    private String email,uid,name,gender,mobileno;
+
+    public User(String email, String uid, String name, String gender, String mobileno) {
+        this.email = email;
+        this.uid = uid;
+        this.name = name;
+        this.gender = gender;
+        this.mobileno = mobileno;
+    }
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMobileno() {
+        return mobileno;
+    }
+
+    public void setMobileno(String mobileno) {
+        this.mobileno = mobileno;
+    }
 }
