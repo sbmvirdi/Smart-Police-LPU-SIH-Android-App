8a18b625ea338a63d85f542955df46945d8d65e6
Shubam Virdi <Shubhamvirdic3@gmail.com>
2020-01-18 17:30:18 +0530

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/sihlpu/smartpolice/Activities/MainActivity.java
+++ app/src/main/java/com/sihlpu/smartpolice/Activities/MainActivity.java
@@ -1,16 +1,28 @@
 package com.sihlpu.smartpolice.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
+import com.sihlpu.smartpolice.Adapters.SliderAdapter;
 import com.sihlpu.smartpolice.R;
+import com.smarteist.autoimageslider.SliderView;
 
 public class MainActivity extends AppCompatActivity {
+    private SliderView mSliderView;
+    private CardView fircard,StolenRecoveredcard,knowyourpolice,knowyourstate,noc,verifications,status,stateservices;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mSliderView = findViewById(R.id.imageSlider);
+
+        SliderAdapter adapter = new SliderAdapter(this);
+        mSliderView.setSliderAdapter(adapter);
     }
 }
