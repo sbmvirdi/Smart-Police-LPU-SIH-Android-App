da0cd91f712af040bf3bc204ac6384c1f9068450
ShaikAbdulRazzaaq <43449008+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-01-18 16:39:38 +0530

    loginUpdatedwithSplashScreen

--- app/src/main/java/com/sihlpu/smartpolice/Activities/MainActivity.java
+++ app/src/main/java/com/sihlpu/smartpolice/Activities/MainActivity.java
@@ -2,7 +2,9 @@ package com.sihlpu.smartpolice.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.sihlpu.smartpolice.R;
 
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        }, 2000);
     }
 }
---
+++ app/src/main/java/com/sihlpu/smartpolice/Activities/OptVerification.java
@@ -0,0 +1,16 @@
+package com.sihlpu.smartpolice.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sihlpu.smartpolice.R;
+
+public class OptVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opt_verification);
+    }
+}
