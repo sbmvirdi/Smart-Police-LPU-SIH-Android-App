dba0ca11603ba9ff9d8fd33eca37003cd73a621e
ShaikAbdulRazzaaq <43449008+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-01-18 15:10:56 +0530

    loginpageUpdated

--- app/src/main/java/com/sihlpu/smartpolice/Activities/LoginActivity.java
+++ app/src/main/java/com/sihlpu/smartpolice/Activities/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.sihlpu.smartpolice.R;
 
@@ -19,5 +20,8 @@ Button login,signUp;
         password=findViewById(R.id.password);
         login=findViewById(R.id.button_login);
         signUp=findViewById(R.id.button_signup);
+        if(username.getText().toString().isEmpty()||password.getText().toString().isEmpty()){
+            Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+        }
     }
 }
